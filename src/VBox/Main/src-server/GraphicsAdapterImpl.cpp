a032de23cfb165fbc297693600a8aa8edf1b6cc2
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-09-18 13:02:12 +0000

    Main: Fixed enabling 3D when selecting supported graphics controllers (static not needed/possible here). bugref:10749
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@106081 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/GraphicsAdapterImpl.cpp
+++ src/VBox/Main/src-server/GraphicsAdapterImpl.cpp
@@ -503,19 +503,19 @@ void GraphicsAdapter::i_updateFeatures()
         GraphicsFeature_T enmFeature;
     };
 
-    static FEATUREMEMBER2ENUM s_aFeatures[] =
+    FEATUREMEMBER2ENUM aFeatures[] =
     {
         { &mData->fAccelerate2DVideo, GraphicsFeature_Acceleration2DVideo },
         { &mData->fAccelerate3D,      GraphicsFeature_Acceleration3D },
     };
 
-    for (size_t i = 0; i < RT_ELEMENTS(s_aFeatures); i++)
+    for (size_t i = 0; i < RT_ELEMENTS(aFeatures); i++)
     {
-        if (*s_aFeatures[i].pfFeatureMember)
-            *s_aFeatures[i].pfFeatureMember
+        if (*aFeatures[i].pfFeatureMember)
+            *aFeatures[i].pfFeatureMember
                 = PlatformProperties::s_isGraphicsControllerFeatureSupported(mParent->i_getPlatform()->i_getArchitecture(),
                                                                              mData->graphicsControllerType,
-                                                                             s_aFeatures[i].enmFeature);
+                                                                             aFeatures[i].enmFeature);
     }
 }
 

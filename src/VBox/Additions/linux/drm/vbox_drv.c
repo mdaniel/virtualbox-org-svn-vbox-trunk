baf7c2caf502349b44a22f43be0554650832f6b4
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-10-01 13:31:33 +0000

    Additions: Linux: vboxvideo: Introduce initial support for kernel 6.12, bugref:10782.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@106194 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/drm/vbox_drv.c
+++ src/VBox/Additions/linux/drm/vbox_drv.c
@@ -359,7 +359,9 @@ static struct drm_driver driver = {
 	.load = vbox_driver_load,
 	.unload = vbox_driver_unload,
 #endif
+#if RTLNX_VER_MAX(6,12,0)
 	.lastclose = vbox_driver_lastclose,
+#endif
 	.master_set = vbox_master_set,
 	.master_drop = vbox_master_drop,
 #if RTLNX_VER_MIN(3,18,0) || RTLNX_RHEL_MAJ_PREREQ(7,2)

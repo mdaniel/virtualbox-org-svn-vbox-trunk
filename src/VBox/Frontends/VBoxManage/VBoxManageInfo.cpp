084b3fd1f9d907e7049defbe79270dd0f333b8f0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-10-02 21:05:12 +0000

    VBoxManage: Fix breakage of "list vms" and "showvminfo" for inaccessible VMs due to misplaced API calls. The inaccessible VM checking must be done first, otherwise just meaningless errors are shown. bugref:10384
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@106209 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
+++ src/VBox/Frontends/VBoxManage/VBoxManageInfo.cpp
@@ -1112,17 +1112,6 @@ HRESULT showVMInfo(ComPtr<IVirtualBox> pVirtualBox,
     if (pSession)
         pSession->COMGETTER(Console)(pConsole.asOutParam());
 
-    ComPtr<IPlatform> platform;
-    CHECK_ERROR_RET(machine, COMGETTER(Platform)(platform.asOutParam()), hrc);
-    ComPtr<IPlatformProperties> platformProperties;
-    CHECK_ERROR_RET(platform, COMGETTER(Properties)(platformProperties.asOutParam()), hrc);
-
-    PlatformArchitecture_T platformArch;
-    CHECK_ERROR_RET(platform, COMGETTER(Architecture)(&platformArch), hrc);
-
-    ComPtr<IFirmwareSettings> firmwareSettings;
-    CHECK_ERROR_RET(machine, COMGETTER(FirmwareSettings)(firmwareSettings.asOutParam()), hrc);
-
     char szNm[80];
     char szValue[256];
 
@@ -1184,6 +1173,17 @@ HRESULT showVMInfo(ComPtr<IVirtualBox> pVirtualBox,
         }
         return S_OK;
     }
+
+    ComPtr<IPlatform> platform;
+    CHECK_ERROR_RET(machine, COMGETTER(Platform)(platform.asOutParam()), hrc);
+    ComPtr<IPlatformProperties> platformProperties;
+    CHECK_ERROR_RET(platform, COMGETTER(Properties)(platformProperties.asOutParam()), hrc);
+
+    PlatformArchitecture_T platformArch;
+    CHECK_ERROR_RET(platform, COMGETTER(Architecture)(&platformArch), hrc);
+
+    ComPtr<IFirmwareSettings> firmwareSettings;
+    CHECK_ERROR_RET(machine, COMGETTER(FirmwareSettings)(firmwareSettings.asOutParam()), hrc);
 
     if (details == VMINFO_COMPACT)
     {

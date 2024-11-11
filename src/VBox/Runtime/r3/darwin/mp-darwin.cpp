1d8a727dfff8cdc5549801ea7a59d6e48d2d9fd0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-11-11 15:18:06 +0000

    IPRT/mp-darwin.cpp: some musings regarding RTMpCpuId
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@106928 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/darwin/mp-darwin.cpp
+++ src/VBox/Runtime/r3/darwin/mp-darwin.cpp
@@ -143,6 +143,9 @@ RTDECL(RTCPUID) RTMpCpuId(void)
 {
     /* xnu-7195.50.7.100.1/osfmk/arm64/start.s and machine_routines.c sets TPIDRRO_EL0
        to the cpu_data_t::cpu_id value. */
+/** @todo r=bird: set_thread_register() in cswitch.s OTOH, applies MACHDEP_CPUNUM_MASK to 
+ * the value before taking it as the CPU number.  In libsyscall/os/tsd.h it is masked by
+ * the same value in _os_cpu_number(). */
     uint64_t u64Ret;
     __asm__ __volatile__("mrs %0,TPIDRRO_EL0\n\t" : "=r" (u64Ret));
     return (RTCPUID)u64Ret;

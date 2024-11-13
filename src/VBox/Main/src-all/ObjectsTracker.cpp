0dd99165c045b59628ca3f1dba3eb1b972530a6d
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-11-13 09:17:58 +0000

    Small fix the error handling.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@106979 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-all/ObjectsTracker.cpp
+++ src/VBox/Main/src-all/ObjectsTracker.cpp
@@ -338,10 +338,10 @@ HRESULT TrackedObjectsCollector::getObj(const com::Utf8Str &aObjId,
         /* Update some fields in the found object if needed. in instance, the last access time */
         com::Utf8Str lat = pIter->second.updateLastAccessTime(); /* Update the access time */
         Log2(("The updated last access time is %s\n", lat.c_str()));
-        vrc = VINF_SUCCESS;
+        hrc = S_OK;
     }
 
-    if (RT_SUCCESS(vrc)) /** @todo r=bird: This won't ever fail. Did you mixup vrc and hrc above? */
+    if (SUCCEEDED(hrc))
     {
         /** @todo r=bird: Why do three lookups? */
         if ( i_getObj(aObjId).getInterface().isNotNull() )

32fd01845402c364b4192bc10736769e2989ff31
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-11-18 21:19:23 +0000

    iprt: Linux: Add initial support for RHEL 9.6 kernel (fix for pre 6.5 kernels), bugref:10482.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107034 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
+++ src/VBox/Runtime/r0drv/linux/memobj-r0drv-linux.c
@@ -655,7 +655,11 @@ static int rtR0MemObjLinuxVMap(PRTR0MEMOBJLNX pMemLnx, bool fExecutable)
 # if defined(RT_ARCH_ARM64)
         /* ARM64 architecture has no _PAGE_NX, _PAGE_PRESENT and _PAGE_RW flags.
          * Closest alternatives would be PTE_PXN, PTE_UXN, PROT_DEFAULT and PTE_WRITE. */
+#  if RTLNX_VER_MIN(6,5,0)
         pgprot_val(fPg) = _PAGE_KERNEL; /* (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_WRITE | PTE_ATTRINDX(MT_NORMAL). */
+#  else /* < 6.5.0 */
+        pgprot_val(fPg) = PROT_NORMAL; /* (PROT_DEFAULT | PTE_PXN | PTE_UXN | PTE_WRITE | PTE_ATTRINDX(MT_NORMAL). */
+#  endif /* 6.5.0 */
 # else /* !RT_ARCH_ARM64 */
         pgprot_val(fPg) = _PAGE_PRESENT | _PAGE_RW;
 #  ifdef _PAGE_NX

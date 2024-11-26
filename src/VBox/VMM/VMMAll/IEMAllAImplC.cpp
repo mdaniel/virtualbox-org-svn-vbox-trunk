40189213b7808439900e87d4c3894441042fe1ee
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-11-26 10:32:16 +0000

    VMM/IEM: fix IEM vpermd instruction emulation; bugref:9898; jiraref:VBP-1464
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107143 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
+++ src/VBox/VMM/VMMAll/IEMAllAImplC.cpp
@@ -20862,14 +20862,14 @@ IEM_DECL_IMPL_DEF(void, iemAImpl_vpermpd_u256_fallback,(PRTUINT256U puDst, PCRTU
  */
 IEM_DECL_IMPL_DEF(void, iemAImpl_vpermd_u256_fallback,(PRTUINT256U puDst, PCRTUINT256U puSrc1, PCRTUINT256U puSrc2))
 {
-    puDst->au32[0] = puSrc2->au32[puSrc1->au32[0] & 0x3];
-    puDst->au32[1] = puSrc2->au32[puSrc1->au32[1] & 0x3];
-    puDst->au32[2] = puSrc2->au32[puSrc1->au32[2] & 0x3];
-    puDst->au32[3] = puSrc2->au32[puSrc1->au32[3] & 0x3];
-    puDst->au32[4] = puSrc2->au32[puSrc1->au32[4] & 0x3];
-    puDst->au32[5] = puSrc2->au32[puSrc1->au32[5] & 0x3];
-    puDst->au32[6] = puSrc2->au32[puSrc1->au32[6] & 0x3];
-    puDst->au32[7] = puSrc2->au32[puSrc1->au32[7] & 0x3];
+    puDst->au32[0] = puSrc2->au32[puSrc1->au32[0] & 0x7];
+    puDst->au32[1] = puSrc2->au32[puSrc1->au32[1] & 0x7];
+    puDst->au32[2] = puSrc2->au32[puSrc1->au32[2] & 0x7];
+    puDst->au32[3] = puSrc2->au32[puSrc1->au32[3] & 0x7];
+    puDst->au32[4] = puSrc2->au32[puSrc1->au32[4] & 0x7];
+    puDst->au32[5] = puSrc2->au32[puSrc1->au32[5] & 0x7];
+    puDst->au32[6] = puSrc2->au32[puSrc1->au32[6] & 0x7];
+    puDst->au32[7] = puSrc2->au32[puSrc1->au32[7] & 0x7];
 }
 
 

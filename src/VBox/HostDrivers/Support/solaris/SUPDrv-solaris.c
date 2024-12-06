4f262fa3c3de1a01593051f92fbb3d55d3c7c05a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-12-06 23:58:26 +0000

    Add,HostDrivers: More parfait/solaris kludges. [fix]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107252 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/solaris/SUPDrv-solaris.c
+++ src/VBox/HostDrivers/Support/solaris/SUPDrv-solaris.c
@@ -40,7 +40,7 @@
 *********************************************************************************************************************************/
 #define LOG_GROUP LOG_GROUP_SUP_DRV
 #ifdef VBOX_WITH_PARFAIT
-# include <iprt/stdint.h> /* HACK ALERT! Contains workaround for int_fast16_t & uint_fast16_t clash. */
+# include <iprt/types.h> /* HACK ALERT! Contains workaround for int_fast16_t & uint_fast16_t clash. */
 #endif
 #include <sys/types.h>
 #include <sys/param.h>

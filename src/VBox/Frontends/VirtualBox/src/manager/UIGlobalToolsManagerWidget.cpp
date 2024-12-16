46f2280b00b5c53c8ab042eeba51f552575741f5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2024-12-16 17:43:57 +0000

    FE/Qt: bugref:10814: VBox Manager: Clone out Global Tool Manager code from the Advanced VBox Manager widget; s.a. r166332.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107337 cfe28804-0f27-0410-a406-dd0f0b0b656f

---
+++ src/VBox/Frontends/VirtualBox/src/manager/UIGlobalToolsManagerWidget.cpp
@@ -0,0 +1,348 @@
+/* $Id$ */
+/** @file
+ * VBox Qt GUI - UIGlobalToolsManagerWidget class implementation.
+ */
+
+/*
+ * Copyright (C) 2006-2024 Oracle and/or its affiliates.
+ *
+ * This file is part of VirtualBox base platform packages, as
+ * available from https://www.virtualbox.org.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+/* Qt includes: */
+// #include <QApplication>
+#include <QHBoxLayout>
+// #include <QStyle>
+// #include <QToolButton>
+// #include <QVBoxLayout>
+
+/* GUI includes: */
+#include "UIActionPoolManager.h"
+#include "UIChooser.h"
+#include "UICommon.h"
+#include "UIExtraDataManager.h"
+// #include "UILoggingDefs.h"
+#include "UIMachineManagerWidget.h"
+// #include "UIMessageCenter.h"
+#include "UINotificationCenter.h"
+// #include "UISlidingAnimation.h"
+// #include "UITabBar.h"
+#include "UIToolPaneGlobal.h"
+#include "UIToolPaneMachine.h"
+#include "UITools.h"
+// #include "UITranslationEventListener.h"
+// #include "UIVirtualBoxEventHandler.h"
+#include "UIVirtualBoxManagerAdvancedWidget.h"
+#include "UIGlobalToolsManagerWidget.h"
+// #include "UIVirtualMachineItemCloud.h"
+// #include "UIVirtualMachineItemLocal.h"
+#ifndef VBOX_WS_MAC
+// # include "UIMenuBar.h"
+#endif
+
+
+UIGlobalToolsManagerWidget::UIGlobalToolsManagerWidget(UIVirtualBoxManagerAdvancedWidget *pParent,
+                                                       UIActionPool *pActionPool)
+    : QWidget(pParent)
+    , m_pActionPool(pActionPool)
+    , m_pMenu(0)
+    , m_pPane(0)
+{
+    prepare();
+}
+
+UIGlobalToolsManagerWidget::~UIGlobalToolsManagerWidget()
+{
+    cleanup();
+}
+
+UIToolType UIGlobalToolsManagerWidget::menuToolType() const
+{
+    AssertPtrReturn(toolMenu(), UIToolType_Invalid);
+    return toolMenu()->toolsType();
+}
+
+void UIGlobalToolsManagerWidget::setMenuToolType(UIToolType enmType)
+{
+    /* Sanity check: */
+    AssertReturnVoid(enmType != UIToolType_Invalid);
+    /* Make sure new tool type is of Global class: */
+    AssertReturnVoid(UIToolStuff::isTypeOfClass(enmType, UIToolClass_Global));
+
+    AssertPtrReturnVoid(toolMenu());
+    toolMenu()->setToolsType(enmType);
+}
+
+UIToolType UIGlobalToolsManagerWidget::toolType() const
+{
+    AssertPtrReturn(toolPane(), UIToolType_Invalid);
+    return toolPane()->currentTool();
+}
+
+bool UIGlobalToolsManagerWidget::isToolOpened(UIToolType enmType) const
+{
+    /* Sanity check: */
+    AssertReturn(enmType != UIToolType_Invalid, false);
+    /* Make sure new tool type is of Global class: */
+    AssertReturn(UIToolStuff::isTypeOfClass(enmType, UIToolClass_Global), false);
+
+    AssertPtrReturn(toolPane(), false);
+    return toolPane()->isToolOpened(enmType);
+}
+
+void UIGlobalToolsManagerWidget::switchToolTo(UIToolType enmType)
+{
+    /* Sanity check: */
+    AssertReturnVoid(enmType != UIToolType_Invalid);
+    /* Make sure new tool type is of Global class: */
+    AssertReturnVoid(UIToolStuff::isTypeOfClass(enmType, UIToolClass_Global));
+
+    /* Open corresponding tool: */
+    AssertPtrReturnVoid(toolPane());
+    toolPane()->openTool(enmType);
+
+    /* Special handling for Machines Global tool,
+     * notify machine tool-pane it's active: */
+    if (enmType == UIToolType_Machines)
+    {
+        toolPane()->setActive(false);
+        toolPaneMachine()->setActive(true);
+    }
+    /* Otherwise, notify global tool-pane it's active: */
+    else
+    {
+        toolPaneMachine()->setActive(false);
+        toolPane()->setActive(true);
+    }
+
+    /* Special handling for Activities Global tool,
+     * start unconditionally updating all cloud VMs: */
+    if (enmType == UIToolType_Activities)
+    {
+        chooser()->setKeepCloudNodesUpdated(true);
+        toolPane()->setCloudMachineItems(chooser()->cloudMachineItems());
+    }
+    /* Otherwise, stop unconditionally updating all cloud VMs,
+     * (tho they will still be updated if selected) */
+    else
+        chooser()->setKeepCloudNodesUpdated(false);
+
+    /* Let the parent know: */
+    emit sigToolTypeChange();
+}
+
+void UIGlobalToolsManagerWidget::closeTool(UIToolType enmType)
+{
+    /* Sanity check: */
+    AssertReturnVoid(enmType != UIToolType_Invalid);
+    /* Make sure new tool type is of Global class: */
+    AssertReturnVoid(UIToolStuff::isTypeOfClass(enmType, UIToolClass_Global));
+
+    AssertPtrReturnVoid(toolPane());
+    toolPane()->closeTool(enmType);
+}
+
+QString UIGlobalToolsManagerWidget::currentHelpKeyword() const
+{
+    AssertPtrReturn(toolPane(), QString());
+    return toolPane()->currentHelpKeyword();
+}
+
+void UIGlobalToolsManagerWidget::sltHandleCommitData()
+{
+    // WORKAROUND:
+    // This will be fixed proper way during session management cleanup for Qt6.
+    // But for now we will just cleanup connections which is Ok anyway.
+    cleanupConnections();
+}
+
+void UIGlobalToolsManagerWidget::sltHandleSettingsExpertModeChange()
+{
+    /* Update tools restrictions: */
+    updateToolsMenuGlobal();
+}
+
+void UIGlobalToolsManagerWidget::sltHandleCloudProfileStateChange(const QString &, const QString &)
+{
+    /* If Global Activities tool is currently chosen: */
+    AssertPtrReturnVoid(toolPane());
+    if (toolType() == UIToolType_Activities)
+    {
+        /* Propagate a set of cloud machine items to Global tool-pane: */
+        toolPane()->setCloudMachineItems(chooser()->cloudMachineItems());
+    }
+}
+
+void UIGlobalToolsManagerWidget::sltHandleToolsMenuIndexChange(UIToolType enmType)
+{
+    switchToolTo(enmType);
+}
+
+void UIGlobalToolsManagerWidget::sltSwitchToActivitiesTool()
+{
+    setMenuToolType(UIToolType_Activities);
+}
+
+void UIGlobalToolsManagerWidget::prepare()
+{
+    /* Prepare everything: */
+    prepareWidgets();
+    prepareConnections();
+
+    /* Load settings: */
+    loadSettings();
+}
+
+void UIGlobalToolsManagerWidget::prepareWidgets()
+{
+    /* Create layout: */
+    QHBoxLayout *pLayout = new QHBoxLayout(this);
+    if (pLayout)
+    {
+        /* Configure layout: */
+        pLayout->setContentsMargins(0, 0, 0, 0);
+
+        /* Create tools-menu: */
+        m_pMenu = new UITools(this, UIToolClass_Global, actionPool());
+        if (toolMenu())
+        {
+            /* Add into layout: */
+            pLayout->addWidget(toolMenu());
+        }
+
+        /* Create tools-pane: */
+        m_pPane = new UIToolPaneGlobal(actionPool());
+        if (toolPane())
+        {
+            /// @todo make sure it's used properly
+            toolPane()->setActive(true);
+
+            /* Add into layout: */
+            pLayout->addWidget(toolPane());
+        }
+    }
+
+    /* Create notification-center: */
+    UINotificationCenter::create(this);
+}
+
+void UIGlobalToolsManagerWidget::prepareConnections()
+{
+    /* UICommon connections: */
+    connect(&uiCommon(), &UICommon::sigAskToCommitData,
+            this, &UIGlobalToolsManagerWidget::sltHandleCommitData);
+
+    /* Global VBox extra-data event handlers: */
+    connect(gEDataManager, &UIExtraDataManager::sigSettingsExpertModeChange,
+            this, &UIGlobalToolsManagerWidget::sltHandleSettingsExpertModeChange);
+
+    /* Chooser-pane connections: */
+    connect(chooser(), &UIChooser::sigCloudProfileStateChange,
+            this, &UIGlobalToolsManagerWidget::sltHandleCloudProfileStateChange);
+
+    /* Tools-menu connections: */
+    connect(toolMenu(), &UITools::sigSelectionChanged,
+            this, &UIGlobalToolsManagerWidget::sltHandleToolsMenuIndexChange);
+
+    /* Tools-pane connections: */
+    connect(toolPaneMachine(), &UIToolPaneMachine::sigSwitchToActivityOverviewPane,
+            this, &UIGlobalToolsManagerWidget::sltSwitchToActivitiesTool);
+}
+
+void UIGlobalToolsManagerWidget::loadSettings()
+{
+    /* Open tool last chosen in tools-menu: */
+    switchToolTo(toolMenu()->toolsType());
+}
+
+void UIGlobalToolsManagerWidget::cleanupConnections()
+{
+    /* Global VBox extra-data event handlers: */
+    disconnect(gEDataManager, &UIExtraDataManager::sigSettingsExpertModeChange,
+               this, &UIGlobalToolsManagerWidget::sltHandleSettingsExpertModeChange);
+
+    /* Chooser-pane connections: */
+    disconnect(chooser(), &UIChooser::sigCloudProfileStateChange,
+               this, &UIGlobalToolsManagerWidget::sltHandleCloudProfileStateChange);
+
+    /* Tools-menu connections: */
+    disconnect(toolMenu(), &UITools::sigSelectionChanged,
+               this, &UIGlobalToolsManagerWidget::sltHandleToolsMenuIndexChange);
+
+    /* Tools-pane connections: */
+    disconnect(toolPaneMachine(), &UIToolPaneMachine::sigSwitchToActivityOverviewPane,
+               this, &UIGlobalToolsManagerWidget::sltSwitchToActivitiesTool);
+}
+
+void UIGlobalToolsManagerWidget::cleanupWidgets()
+{
+    UINotificationCenter::destroy();
+}
+
+void UIGlobalToolsManagerWidget::cleanup()
+{
+    /* Ask sub-dialogs to commit data: */
+    sltHandleCommitData();
+
+    /* Cleanup everything: */
+    cleanupWidgets();
+}
+
+UITools *UIGlobalToolsManagerWidget::toolMenu() const
+{
+    return m_pMenu;
+}
+
+UIToolPaneGlobal *UIGlobalToolsManagerWidget::toolPane() const
+{
+    return m_pPane;
+}
+
+UIMachineManagerWidget *UIGlobalToolsManagerWidget::machineManager() const
+{
+    return toolPane()->machineManager();
+}
+
+UIChooser *UIGlobalToolsManagerWidget::chooser() const
+{
+    return machineManager()->chooser();
+}
+
+UIToolPaneMachine *UIGlobalToolsManagerWidget::toolPaneMachine() const
+{
+    return machineManager()->toolPane();
+}
+
+void UIGlobalToolsManagerWidget::updateToolsMenuGlobal()
+{
+    /* Update global tools restrictions: */
+    QSet<UIToolType> restrictedTypes;
+    const bool fExpertMode = gEDataManager->isSettingsInExpertMode();
+    if (!fExpertMode)
+        restrictedTypes << UIToolType_Media
+                        << UIToolType_Network;
+    if (restrictedTypes.contains(toolMenu()->toolsType()))
+        setMenuToolType(UIToolType_Welcome);
+    const QList restrictions(restrictedTypes.begin(), restrictedTypes.end());
+    toolMenu()->setRestrictedToolTypes(restrictions);
+
+    /* Take restrictions into account, closing all restricted tools: */
+    foreach (const UIToolType &enmRestrictedType, restrictedTypes)
+        toolPane()->closeTool(enmRestrictedType);
+}
---
+++ src/VBox/Frontends/VirtualBox/src/manager/UIGlobalToolsManagerWidget.h
@@ -0,0 +1,177 @@
+/* $Id$ */
+/** @file
+ * VBox Qt GUI - UIGlobalToolsManagerWidget class declaration.
+ */
+
+/*
+ * Copyright (C) 2006-2024 Oracle and/or its affiliates.
+ *
+ * This file is part of VirtualBox base platform packages, as
+ * available from https://www.virtualbox.org.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, in version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#ifndef FEQT_INCLUDED_SRC_manager_UIGlobalToolsManagerWidget_h
+#define FEQT_INCLUDED_SRC_manager_UIGlobalToolsManagerWidget_h
+#ifndef RT_WITHOUT_PRAGMA_ONCE
+# pragma once
+#endif
+
+/* Qt includes: */
+#include <QWidget>
+
+/* GUI includes: */
+#include "UIExtraDataDefs.h"
+
+/* Forward declarations: */
+class UIActionPool;
+class UIChooser;
+class UIMachineManagerWidget;
+class UIToolPaneGlobal;
+class UIToolPaneMachine;
+class UITools;
+class UIVirtualBoxManagerAdvancedWidget;
+
+/** QWidget extension used as VirtualBox Manager Widget instance. */
+class UIGlobalToolsManagerWidget : public QWidget
+{
+    Q_OBJECT;
+
+signals:
+
+    /** @name Tools pane stuff.
+      * @{ */
+        /** Notifies about Tool type change. */
+        void sigToolTypeChange();
+    /** @} */
+
+public:
+
+    /** Constructs VirtualBox Manager widget passing @a pParent to the base-class.
+      * @param  pActionPool  Brings the action-pool reference.  */
+    UIGlobalToolsManagerWidget(UIVirtualBoxManagerAdvancedWidget *pParent, UIActionPool *pActionPool);
+    /** Destructs VirtualBox Manager widget. */
+    virtual ~UIGlobalToolsManagerWidget() RT_OVERRIDE;
+
+    /** @name Common stuff.
+      * @{ */
+        /** Returns the action-pool instance. */
+        UIActionPool *actionPool() const { return m_pActionPool; }
+    /** @} */
+
+    /** @name Tools pane stuff.
+      * @{ */
+        /** Returns menu tool type. */
+        UIToolType menuToolType() const;
+        /** Defines menu tool @a enmType. */
+        void setMenuToolType(UIToolType enmType);
+
+        /** Returns pane tool type. */
+        UIToolType toolType() const;
+        /** Returns whether pane has tool of passed @a enmType. */
+        bool isToolOpened(UIToolType enmType) const;
+        /** Switches tool to passed @a enmType. */
+        void switchToolTo(UIToolType enmType);
+        /** Closes tool of passed @a enmType. */
+        void closeTool(UIToolType enmType);
+    /** @} */
+
+    /** @name Help browser stuff.
+      * @{ */
+        /** Returns the current help key word. */
+        QString currentHelpKeyword() const;
+    /** @} */
+
+private slots:
+
+    /** @name General stuff.
+      * @{ */
+        /** Handles request to commit data. */
+        void sltHandleCommitData();
+    /** @} */
+
+    /** @name CVirtualBox extra-data event handling stuff.
+      * @{ */
+        /** Handles signal about settings expert mode change. */
+        void sltHandleSettingsExpertModeChange();
+    /** @} */
+
+    /** @name Chooser pane stuff.
+      * @{ */
+        /** Handles state change for cloud profile with certain @a strProviderShortName and @a strProfileName. */
+        void sltHandleCloudProfileStateChange(const QString &strProviderShortName,
+                                              const QString &strProfileName);
+    /** @} */
+
+    /** @name Tools pane stuff.
+      * @{ */
+        /** Handles signal about Tools-menu index change.
+          * @param  enmType  Brings current tool type. */
+        void sltHandleToolsMenuIndexChange(UIToolType enmType);
+
+        /** Handles signal requesting switch to Activities tool. */
+        void sltSwitchToActivitiesTool();
+    /** @} */
+
+private:
+
+    /** @name Prepare/Cleanup cascade.
+      * @{ */
+        /** Prepares all. */
+        void prepare();
+        /** Prepares widgets. */
+        void prepareWidgets();
+        /** Prepares connections. */
+        void prepareConnections();
+        /** Loads settings. */
+        void loadSettings();
+
+        /** Cleanups connections. */
+        void cleanupConnections();
+        /** Cleanups widgets. */
+        void cleanupWidgets();
+        /** Cleanups all. */
+        void cleanup();
+    /** @} */
+
+    /** @name Tools stuff.
+      * @{ */
+        /** Returns tool-menu instance. */
+        UITools *toolMenu() const;
+        /** Returns tool-pane instance. */
+        UIToolPaneGlobal *toolPane() const;
+        /** Returns Machine Manager reference. */
+        UIMachineManagerWidget *machineManager() const;
+        /** Returns Machine Manager's Chooser-pane reference. */
+        UIChooser *chooser() const;
+        /** Returns Machine Manager's Tool-pane instance. */
+        UIToolPaneMachine *toolPaneMachine() const;
+
+        /** Updates Global tools menu. */
+        void updateToolsMenuGlobal();
+    /** @} */
+
+    /** Holds the action-pool instance. */
+    UIActionPool *m_pActionPool;
+
+    /** Holds the Global Tools-menu instance. */
+    UITools          *m_pMenu;
+    /** Holds the Global Tools-pane instance. */
+    UIToolPaneGlobal *m_pPane;
+};
+
+#endif /* !FEQT_INCLUDED_SRC_manager_UIGlobalToolsManagerWidget_h */

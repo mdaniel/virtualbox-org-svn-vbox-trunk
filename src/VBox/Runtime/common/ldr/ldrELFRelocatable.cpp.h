6923947b167a3ceb1b5424c00c8c4de1f1466a93
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-10 15:26:53 +0000

    Runtime/common/ldr/ldrELFRelocatable.cpp.: Remove unused variable, bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107668 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/common/ldr/ldrELFRelocatable.cpp.h
+++ src/VBox/Runtime/common/ldr/ldrELFRelocatable.cpp.h
@@ -1428,7 +1428,6 @@ static DECLCALLBACK(int) RTLDRELF_NAME(EnumSegments)(PRTLDRMODINTERNAL pMod, PFN
      * Do the enumeration.
      */
     char            szName[32];
-    Elf_Addr        uPrevMappedRva = 0;
     const Elf_Shdr *paShdrs    = pModElf->paShdrs;
     const Elf_Shdr *paOrgShdrs = pModElf->paOrgShdrs;
     for (unsigned iShdr = pModElf->iFirstSect; iShdr < pModElf->Ehdr.e_shnum; iShdr++)
@@ -1468,7 +1467,6 @@ static DECLCALLBACK(int) RTLDRELF_NAME(EnumSegments)(PRTLDRMODINTERNAL pMod, PFN
                 Seg.cbMapped = pShdr2->sh_addr - paShdrs[iShdr].sh_addr;
             else
                 Seg.cbMapped = pModElf->cbImage - paShdrs[iShdr].sh_addr;
-            uPrevMappedRva = Seg.RVA;
         }
         else
         {

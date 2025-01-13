3f1abba9c26d7e2701eaa1e3f747ce99d9e126bc
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-13 18:17:23 +0000

    src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp: Fixed warnings found by Parfait (unused assignment). jiraref:VBP-1424
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107718 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceVMInfo.cpp
@@ -1849,7 +1849,7 @@ static DECLCALLBACK(int) vbsvcVMInfoWorker(bool volatile *pfShutdown)
             /* Reset event semaphore if it got triggered. */
             rc2 = RTSemEventMultiReset(g_hVMInfoEvent);
             if (RT_FAILURE(rc2))
-                rc2 = VGSvcError("RTSemEventMultiReset failed; rc2=%Rrc\n", rc2);
+                VGSvcError("RTSemEventMultiReset failed; rc2=%Rrc\n", rc2);
         }
     }
 
@@ -1891,7 +1891,7 @@ static DECLCALLBACK(void) vbsvcVMInfoTerm(void)
          *        since it remembers what we've written. */
         /* Delete the "../Net" branch. */
         const char *apszPat[1] = { "/VirtualBox/GuestInfo/Net/*" };
-        int rc = VbglR3GuestPropDelSet(g_uVMInfoGuestPropSvcClientID, &apszPat[0], RT_ELEMENTS(apszPat));
+        VbglR3GuestPropDelSet(g_uVMInfoGuestPropSvcClientID, &apszPat[0], RT_ELEMENTS(apszPat));
 
         /* Destroy LA client info. */
         vgsvcFreeLAClientInfo(&g_LAClientInfo);
@@ -1900,9 +1900,7 @@ static DECLCALLBACK(void) vbsvcVMInfoTerm(void)
         VGSvcPropCacheDestroy(&g_VMInfoPropCache);
 
         /* Disconnect from guest properties service. */
-        rc = VbglR3GuestPropDisconnect(g_uVMInfoGuestPropSvcClientID);
-        if (RT_FAILURE(rc))
-            VGSvcError("Failed to disconnect from guest property service! Error: %Rrc\n", rc);
+        VbglR3GuestPropDisconnect(g_uVMInfoGuestPropSvcClientID);
         g_uVMInfoGuestPropSvcClientID = 0;
 
         RTSemEventMultiDestroy(g_hVMInfoEvent);

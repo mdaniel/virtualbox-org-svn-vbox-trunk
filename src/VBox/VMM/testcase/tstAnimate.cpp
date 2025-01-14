e37667296c124129b3295e86243afd2920804dc8
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-14 10:37:20 +0000

    VMM/testcase/tstAnimate.cpp: Missing error checks, bugref:3409
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107750 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/testcase/tstAnimate.cpp
+++ src/VBox/VMM/testcase/tstAnimate.cpp
@@ -427,7 +427,7 @@ static DECLCALLBACK(int) cfgmR3CreateDefault(PUVM pUVM, PVM pVM, PCVMMR3VTABLE p
     UPDATERC();
     rc = CFGMR3InsertString(pCfg,   "HardDiskDevice",       "piix3ide");
     UPDATERC();
-    rc = CFGMR3InsertString(pCfg,   "FloppyDevice",         "i82078");
+    rc = CFGMR3InsertString(pCfg,   "FloppyDevice",         "i82078");          UPDATERC();
     rc = CFGMR3InsertInteger(pCfg,  "IOAPIC", fIOAPIC);                         UPDATERC();
     RTUUID Uuid;
     RTUuidClear(&Uuid);
@@ -556,7 +556,7 @@ static DECLCALLBACK(int) cfgmR3CreateDefault(PUVM pUVM, PVM pVM, PCVMMR3VTABLE p
     rc = CFGMR3InsertInteger(pInst, "PCIFunctionNo",        0);                     UPDATERC();
     rc = CFGMR3InsertNode(pInst,    "Config", &pCfg);                               UPDATERC();
     rc = CFGMR3InsertInteger(pCfg,  "VRamSize",             8 * _1M);               UPDATERC();
-    rc = CFGMR3InsertInteger(pCfg,  "CustomVideoModes",     0);
+    rc = CFGMR3InsertInteger(pCfg,  "CustomVideoModes",     0);                     UPDATERC();
     rc = CFGMR3InsertInteger(pCfg,  "HeightReduction",      0);                     UPDATERC();
     //rc = CFGMR3InsertInteger(pCfg,  "MonitorCount",         1);                     UPDATERC();
 

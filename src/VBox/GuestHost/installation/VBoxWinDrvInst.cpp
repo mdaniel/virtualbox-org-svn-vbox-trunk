611a3674c6fec63e24c23bfcec29a722538c476a
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-15 09:58:46 +0000

    Windows driver installation/VBoxDrvInst: Renaming (nits).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107765 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
+++ src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
@@ -94,10 +94,10 @@
 #define VBOXWINDRVINST_VALID_RETURN(hDrvInst) VBOXWINDRVINST_VALID_RETURN_RC((hDrvInst), VERR_INVALID_HANDLE)
 
 /** Validates a handle and returns (void) if not valid. */
-#define VBOXWINDRVINST_VALID_RETURN_VOID(hFTPServer) \
+#define VBOXWINDRVINST_VALID_RETURN_VOID(hDrvInst) \
     do { \
-        AssertPtrReturnVoid(hFTPServer); \
-        AssertReturnVoid((hFTPServer)->u32Magic == VBOXWINDRVINST_MAGIC); \
+        AssertPtrReturnVoid(hDrvInst); \
+        AssertReturnVoid((hDrvInst)->u32Magic == VBOXWINDRVINST_MAGIC); \
     } while (0)
 
 

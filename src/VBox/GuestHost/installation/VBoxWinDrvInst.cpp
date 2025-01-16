0126b878d5ca13f97ce7dfeef4d8e680b9240953
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-16 15:09:22 +0000

    Windows driver installation/VBoxWinDrvInst.cpp: Removed a case in vboxWinDrvUninstallFromDriverStore() which never can be reached, minimal reshuffling for better readability.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107822 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
+++ src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
@@ -1686,6 +1686,7 @@ static int vboxWinDrvUninstallFromDriverStore(PVBOXWINDRVINSTINTERNAL pCtx,
         if (g_pfnDiUninstallDriverW)
         {
             vboxWinDrvInstLogVerbose(pCtx, 1, "Using DiUninstallDriverW()");
+
             BOOL fReboot = FALSE;
             if (!(pParms->fFlags & VBOX_WIN_DRIVERINSTALL_F_DRYRUN))
                 fRc = g_pfnDiUninstallDriverW(NULL /* hWndParent */, pCur->wszInfFile, 0 /* Flags */, &fReboot);
@@ -1707,14 +1708,15 @@ static int vboxWinDrvUninstallFromDriverStore(PVBOXWINDRVINSTINTERNAL pCtx,
         {
             vboxWinDrvInstLogVerbose(pCtx, 1, "Using SetupUninstallOEMInfW()");
 
-            DWORD dwUninstallFlags = 0;
-            if (pParms->fFlags & VBOX_WIN_DRIVERINSTALL_F_FORCE)
-                dwUninstallFlags |= SUOI_FORCEDELETE;
-
             if (!(pParms->fFlags & VBOX_WIN_DRIVERINSTALL_F_DRYRUN))
+            {
+                DWORD dwUninstallFlags = 0;
+                if (pParms->fFlags & VBOX_WIN_DRIVERINSTALL_F_FORCE)
+                    dwUninstallFlags |= SUOI_FORCEDELETE;
+
+                /* Takes the oemXXX.inf file (without a path), as found in the Windows INF directory. */
                 fRc = g_pfnSetupUninstallOEMInfW(pCur->wszInfFile, dwUninstallFlags, NULL /* pReserved */);
-            else
-                fRc = FALSE;
+            }
         }
 
         int rc2 = VINF_SUCCESS;

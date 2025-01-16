ec3126bcda742e01b1b7c33894a98f574a9571fb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-16 10:32:43 +0000

    VMM/CPUM: Fixed some incorrect Log/LogRel format specifiers (fixed typo)
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107807 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUM.cpp
+++ src/VBox/VMM/VMMR3/CPUM.cpp
@@ -3958,7 +3958,7 @@ static int cpumR3MapMtrrs(PVM pVM)
         Assert(MtrrMap.idxMtrr == cMtrrsMappable);
         Assert(MtrrMap.idxMtrr == MtrrMap.cMtrrs);
         uint64_t const cbLost = cbRam - MtrrMap.cbMapped;
-        LogRel(("CPUM: WARNING! Could not map %\Rhcb (%RU64 bytes) of RAM using %u variable-range MTRRs\n", cbLost, cbLost,
+        LogRel(("CPUM: WARNING! Could not map %Rhcb (%RU64 bytes) of RAM using %u variable-range MTRRs\n", cbLost, cbLost,
                 MtrrMap.cMtrrs));
     }
 

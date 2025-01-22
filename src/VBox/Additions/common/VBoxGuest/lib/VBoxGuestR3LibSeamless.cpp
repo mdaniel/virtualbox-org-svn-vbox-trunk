fadedcb4297dfbbcf29c86f4fbaa7fb045d83dc3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-22 10:42:54 +0000

    Additions/common/VBoxGuest/lib/VBoxGuestR3LibSeamless.cpp: Fixed warnings found by Parfait (impossible or redundant condition). jiraref:VBP-1424
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107885 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibSeamless.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibSeamless.cpp
@@ -182,7 +182,7 @@ VBGLR3DECL(int) VbglR3SeamlessSendRects(uint32_t cRects, PRTRECT pRects)
 
 VBGLR3DECL(int) VbglR3SeamlessSendMonitorPositions(uint32_t cPositions, PRTPOINT pPositions)
 {
-    if (!pPositions || cPositions <= 0)
+    if (!pPositions || !cPositions)
         return VERR_INVALID_PARAMETER;
 
     VMMDevVideoUpdateMonitorPositions *pReq;
@@ -195,8 +195,7 @@ VBGLR3DECL(int) VbglR3SeamlessSendMonitorPositions(uint32_t cPositions, PRTPOINT
     if (RT_SUCCESS(rc))
     {
         pReq->cPositions = cPositions;
-        if (cPositions)
-            memcpy(&pReq->aPositions, pPositions, cPositions * sizeof(RTPOINT));
+        memcpy(&pReq->aPositions, pPositions, cPositions * sizeof(RTPOINT));
         rc = vbglR3GRPerform(&pReq->header);
         LogFunc(("Monitor position update request returned %Rrc, internal %Rrc.\n",
                  rc, pReq->header.rc));

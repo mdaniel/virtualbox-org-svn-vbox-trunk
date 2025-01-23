8c8c2a09e4d17642c1485345e612a83ccb456aa1
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-01-23 14:28:04 +0000

    Additions: Linux: rcvboxadd: Handle waiting time better in 'reload' and 'status-kernel' (scm fix), bugref:10359.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@107913 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/linux/installer/vboxadd.sh
+++ src/VBox/Additions/linux/installer/vboxadd.sh
@@ -1266,7 +1266,7 @@ reload)
     check_root
     # reload() we will call modprobe(8) in order to reload kernel
     # modules. This operation is asynchronous and requires some time for
-    # modules to be loaded in most of the cases. By setting this variable, we 
+    # modules to be loaded in most of the cases. By setting this variable, we
     # ask check_status_kernel() to wait a bit before making a decision
     # whether modules were loaded or not.
     vbox_add_wait=1

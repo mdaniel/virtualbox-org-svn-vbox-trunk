7fd7be2c6dbf941e2581462ebfa01d922cec0fb6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-02-05 10:58:34 +0000

    FE/Qt: bugref:10814: VBox Manager / Tools pane: More visible background for the current tool selection token.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108063 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/tools/UIToolsItem.cpp
@@ -721,14 +721,14 @@ void UIToolsItem::paintBackground(QPainter *pPainter, const QRect &rectangle) co
                                         : pal.color(QPalette::Disabled, QPalette::Highlight);
             const QColor highlightColor1 = uiCommon().isInDarkMode()
                                          ? highlightColor.lighter(m_iHighlightLightnessStart + 20)
-                                         : highlightColor.darker(m_iHighlightLightnessStart);
+                                         : highlightColor.darker(m_iHighlightLightnessStart + 20);
             const QColor highlightColor2 = uiCommon().isInDarkMode()
                                          ? highlightColor.lighter(m_iHighlightLightnessFinal + 20)
-                                         : highlightColor.darker(m_iHighlightLightnessFinal);
+                                         : highlightColor.darker(m_iHighlightLightnessFinal + 20);
 
             /* Prepare token sub-rect: */
             QRect tokenRect(rectangle.topLeft() + QPoint(0, 4),
-                            QSize(4, rectangle.height() - 8));
+                            QSize(5, rectangle.height() - 8));
 
             /* Draw gradient token: */
             QLinearGradient hlGrad(tokenRect.topLeft(), tokenRect.bottomLeft());

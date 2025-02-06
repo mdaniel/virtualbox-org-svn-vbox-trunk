97ff4e03c1833670426d595d5bfa669c5a0f7ed0
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-02-06 08:58:45 +0000

    Windows driver installation: Logging nits. bugref:10762
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108092 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
+++ src/VBox/GuestHost/installation/VBoxWinDrvInst.cpp
@@ -2010,10 +2010,10 @@ int VBoxWinDrvInstCreateEx(PVBOXWINDRVINST phDrvInst, unsigned uVerbosity, PFNVB
         /* else not fatal. */
 
         vboxWinDrvInstLogInfo(pCtx, VBOX_PRODUCT " Version " VBOX_VERSION_STRING " - r%s", RTBldCfgRevisionStr());
-        vboxWinDrvInstLogInfo(pCtx, "Detected Windows version %d.%d.%d (%s)", RTSYSTEM_NT_VERSION_GET_MAJOR(pCtx->uOsVer),
-                                                                              RTSYSTEM_NT_VERSION_GET_MINOR(pCtx->uOsVer),
-                                                                              RTSYSTEM_NT_VERSION_GET_BUILD(pCtx->uOsVer),
-                                                                              RTBldCfgTargetArch());
+        vboxWinDrvInstLogInfo(pCtx, "Using Windows version %d.%d.%d (%s)", RTSYSTEM_NT_VERSION_GET_MAJOR(pCtx->uOsVer),
+                                                                           RTSYSTEM_NT_VERSION_GET_MINOR(pCtx->uOsVer),
+                                                                           RTSYSTEM_NT_VERSION_GET_BUILD(pCtx->uOsVer),
+                                                                           RTBldCfgTargetArch());
 
         rc = RTOnce(&g_vboxWinDrvInstResolveOnce, vboxWinDrvInstResolveOnce, pCtx);
         if (RT_SUCCESS(rc))
@@ -2113,8 +2113,8 @@ void VBoxWinDrvInstSetOsVersion(VBOXWINDRVINST hDrvInst, uint64_t uOsVer)
 
     pCtx->uOsVer = uOsVer;
 
-    vboxWinDrvInstLogInfo(pCtx, "Set OS version to: %u.%u\n", RTSYSTEM_NT_VERSION_GET_MAJOR(pCtx->uOsVer),
-                                                              RTSYSTEM_NT_VERSION_GET_MINOR(pCtx->uOsVer));
+    vboxWinDrvInstLogInfo(pCtx, "Set OS version to: %u.%u", RTSYSTEM_NT_VERSION_GET_MAJOR(pCtx->uOsVer),
+                                                            RTSYSTEM_NT_VERSION_GET_MINOR(pCtx->uOsVer));
 }
 
 /**
@@ -2608,4 +2608,3 @@ void VBoxWinDrvInstTestParmsDestroy(PVBOXWINDRVINSTPARMS pParms)
     vboxWinDrvInstParmsDestroy(pParms);
 }
 #endif /* TESTCASE */
-

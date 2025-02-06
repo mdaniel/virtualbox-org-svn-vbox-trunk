3c7ee380b80baca428e6d9b1446499924eedc7ec
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-02-06 17:26:46 +0000

    FE/Qt: bugref:10814: VBox Manager: Reordering New/Add toolbar actions for advanced widget mode.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108107 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerAdvancedWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIVirtualBoxManagerAdvancedWidget.cpp
@@ -493,13 +493,13 @@ void UIVirtualBoxManagerAdvancedWidget::updateToolbar()
     {
         case UIToolType_Home:
         {
+            m_pToolBar->addAction(actionPool()->action(UIActionIndexMN_M_Home_S_New));
+            m_pToolBar->addAction(actionPool()->action(UIActionIndexMN_M_Home_S_Add));
+            m_pToolBar->addSeparator();
             m_pToolBar->addAction(actionPool()->action(UIActionIndex_M_Application_S_Preferences));
             m_pToolBar->addSeparator();
             m_pToolBar->addAction(actionPool()->action(UIActionIndexMN_M_File_S_ImportAppliance));
             m_pToolBar->addAction(actionPool()->action(UIActionIndexMN_M_File_S_ExportAppliance));
-            m_pToolBar->addSeparator();
-            m_pToolBar->addAction(actionPool()->action(UIActionIndexMN_M_Home_S_New));
-            m_pToolBar->addAction(actionPool()->action(UIActionIndexMN_M_Home_S_Add));
             break;
         }
         case UIToolType_Extensions:

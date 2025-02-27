407ebcb65ed54f42dd26e512dda39eb5a0391d9c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-02-27 07:22:56 +0000

    VMM: scm fixes, bugref:10392
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108403 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/GICR3Nem-win.cpp
+++ src/VBox/VMM/VMMR3/GICR3Nem-win.cpp
@@ -396,7 +396,7 @@ static DECLCALLBACK(int) gicR3HvSaveExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM)
     AssertLogRelMsgReturn(pState->bVersion == MY_WHV_GLOBAL_INTERRUPT_CONTROLLER_STATE_VERSION,
                           ("WHvGetVirtualProcessorState(%p, WHV_ANY_VP, WHvVirtualProcessorStateTypeGlobalInterruptState,) -> bVersion=%u vs expected=%u\n",
                            pVM->nem.s.hPartition, pState->bVersion, MY_WHV_GLOBAL_INTERRUPT_CONTROLLER_STATE_VERSION)
-                          , VERR_NEM_GET_REGISTERS_FAILED); 
+                          , VERR_NEM_GET_REGISTERS_FAILED);
 
     if (SUCCEEDED(hrc))
     {
@@ -429,7 +429,7 @@ static DECLCALLBACK(int) gicR3HvSaveExec(PPDMDEVINS pDevIns, PSSMHANDLE pSSM)
         AssertLogRelMsgReturn(LocalState.bVersion == MY_WHV_LOCAL_INTERRUPT_CONTROLLER_STATE_VERSION,
                               ("WHvGetVirtualProcessorState(%p, %u, WHvVirtualProcessorStateTypeInterruptControllerState2,) -> bVersion=%u vs expected=%u\n",
                                pVM->nem.s.hPartition, idCpu, LocalState.bVersion, MY_WHV_LOCAL_INTERRUPT_CONTROLLER_STATE_VERSION)
-                              , VERR_NEM_GET_REGISTERS_FAILED);  
+                              , VERR_NEM_GET_REGISTERS_FAILED);
 
         pHlp->pfnSSMPutStruct(pSSM, (const void *)&LocalState, &g_aWHvGicLocalInterruptState[0]);
 

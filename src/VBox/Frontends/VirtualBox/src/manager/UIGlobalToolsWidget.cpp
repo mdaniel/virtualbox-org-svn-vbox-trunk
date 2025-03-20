97793bbce4ecf43d30acc289e3abe6b443ffb8a5
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-03-20 16:54:08 +0000

    FE/Qt: bugref:10814: VBox Manager: Forgot something in r168077.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108665 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/UIGlobalToolsWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/UIGlobalToolsWidget.cpp
@@ -316,11 +316,13 @@ void UIGlobalToolsWidget::sltSwitchToVMActivityTool(const QUuid &uMachineId)
 {
     AssertPtrReturnVoid(chooser());
     chooser()->setCurrentMachine(uMachineId);
+    setMenuToolType(UIToolType_Machines);
     setMenuToolType(UIToolType_VMActivity);
 }
 
 void UIGlobalToolsWidget::sltSwitchToActivitiesTool()
 {
+    setMenuToolType(UIToolType_Managers);
     setMenuToolType(UIToolType_Activities);
 }
 
@@ -389,12 +391,12 @@ void UIGlobalToolsWidget::prepareConnections()
             this, &UIGlobalToolsWidget::sltHandleGlobalToolMenuUpdate);
     connect(this, &UIGlobalToolsWidget::sigToolMenuUpdate,
             this, &UIGlobalToolsWidget::sltHandleManagementToolMenuUpdate);
-    connect(toolPane(), &UIToolPane::sigSwitchToMachineActivityPane,
-            this, &UIGlobalToolsWidget::sltSwitchToVMActivityTool);
     connect(machineToolsWidget(), &UIMachineToolsWidget::sigToolMenuUpdate,
             this, &UIGlobalToolsWidget::sltHandleMachineToolMenuUpdate);
     connect(toolPaneMachine(), &UIToolPane::sigSwitchToActivityOverviewPane,
             this, &UIGlobalToolsWidget::sltSwitchToActivitiesTool);
+    connect(toolPaneManagement(), &UIToolPane::sigSwitchToMachineActivityPane,
+            this, &UIGlobalToolsWidget::sltSwitchToVMActivityTool);
 }
 
 void UIGlobalToolsWidget::loadSettings()

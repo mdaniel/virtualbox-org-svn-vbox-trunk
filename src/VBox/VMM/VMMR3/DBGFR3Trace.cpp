dc95ccc1716d4b4fc0048967b6feb6c372d63ecb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-03-20 18:12:16 +0000

    VMM/VMMR3/DBGFR3Trace.cpp: Make it work for hosts where we don't know the host page size when building, bugref:10391
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108672 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/DBGFR3Trace.cpp
+++ src/VBox/VMM/VMMR3/DBGFR3Trace.cpp
@@ -44,6 +44,7 @@
 
 #include <iprt/assert.h>
 #include <iprt/ctype.h>
+#include <iprt/system.h>
 #include <iprt/trace.h>
 
 
@@ -124,10 +125,10 @@ static int dbgfR3TraceEnable(PVM pVM, uint32_t cbEntry, uint32_t cEntries)
      * Note! We ASSUME that the returned trace buffer handle has the same value
      *       as the heap block.
      */
-    cbBlock = RT_ALIGN_Z(cbBlock, HOST_PAGE_SIZE);
+    cbBlock = RT_ALIGN_Z(cbBlock, RTSystemGetPageSize());
     RTR0PTR pvBlockR0 = NIL_RTR0PTR;
     void   *pvBlockR3 = NULL;
-    rc = SUPR3PageAllocEx(cbBlock >> HOST_PAGE_SHIFT, 0, &pvBlockR3, &pvBlockR0, NULL);
+    rc = SUPR3PageAllocEx(cbBlock >> RTSystemGetPageShift(), 0, &pvBlockR3, &pvBlockR0, NULL);
     if (RT_FAILURE(rc))
         return rc;
 

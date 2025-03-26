f84f7a857341d20e21e31a9b46839be74c939d79
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-03-26 12:49:44 +0000

    VBoxService/Shared Folders: Skip initializing / enabling the 'automount' sub service on <= NT4 guests, as we don't support Shared Folders on those [build fix].
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108755 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
+++ src/VBox/Additions/common/VBoxService/VBoxServiceAutoMount.cpp
@@ -209,8 +209,11 @@ static DECLCALLBACK(int) vbsvcAutomounterInit(void)
     {
         /* If the service was not found, we disable this service without
            causing VBoxService to fail. */
-        if (   rc == VERR_HGCM_SERVICE_NOT_FOUND                          /* Host service is not available. */
-            || RTSystemGetNtVersion() <= RTSYSTEM_MAKE_NT_VERSION(4,0,0)) /* On <= NT4 guests no Shared Folders are available. */
+        if (   rc == VERR_HGCM_SERVICE_NOT_FOUND                         /* Host service is not available. */
+#ifdef RT_OS_WINDOWS
+            || RTSystemGetNtVersion() <= RTSYSTEM_MAKE_NT_VERSION(4,0,0) /* On <= NT4 guests no Shared Folders are available. */
+#endif
+           )
         {
             VGSvcVerbose(0, "vbsvcAutomounterInit: Shared Folders service is not available\n");
             rc = VERR_SERVICE_DISABLED;

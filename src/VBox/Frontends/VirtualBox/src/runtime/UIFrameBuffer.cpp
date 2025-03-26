712aa53c9665a4645bbd360a01f78d9ae4780a6f
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-03-26 17:11:47 +0000

    FE/Qt: ticketref:22096: Runtime UI / Frame-buffer: Make sure mouse cursor (if received from the guest with no mouse pointer integration) is properly scaled according to GUI scale-factor; This patch is provided by JHRobotics.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108760 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBuffer.cpp
+++ src/VBox/Frontends/VirtualBox/src/runtime/UIFrameBuffer.cpp
@@ -1401,7 +1401,7 @@ void UIFrameBufferPrivate::paintDefault(QPaintEvent *pEvent)
         cursorPixmap.setDevicePixelRatio(devicePixelRatio());
 
         /* Draw sub-pixmap: */
-        painter.drawPixmap(m_cursorRectangle.topLeft(), cursorPixmap);
+        painter.drawPixmap(m_cursorRectangle, cursorPixmap);
     }
 }
 

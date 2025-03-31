f68c005184755cb092e8c8836f436201faab070b
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-03-31 14:26:22 +0000

    Devices/EFI/FirmwareNew/BaseTools: Try fix builds on Windows, bugref:4643
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108799 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/EFI/FirmwareNew/BaseTools/Source/C/GenFfs/GenFfs.c
+++ src/VBox/Devices/EFI/FirmwareNew/BaseTools/Source/C/GenFfs/GenFfs.c
@@ -7,9 +7,13 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #ifndef __GNUC__
+# ifdef VBOX
+// RUNTIME_FUNCTION typedef conflicts; just use EFI headers
+# else
 #define RUNTIME_FUNCTION  _WINNT_DUP_RUNTIME_FUNCTION
 #include <windows.h>
 #undef RUNTIME_FUNCTION
+# endif
 #include <io.h>
 #include <sys/types.h>
 #include <sys/stat.h>

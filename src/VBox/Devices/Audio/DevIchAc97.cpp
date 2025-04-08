6d86387ba3dfd967e3f55d6487a47722f19b1b30
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-04-08 14:01:35 +0000

    Audio/DevIchAc97.cpp: Fixed error path in ichac97R3AttachInternal().
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@108889 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Audio/DevIchAc97.cpp
+++ src/VBox/Devices/Audio/DevIchAc97.cpp
@@ -4219,7 +4219,7 @@ static int ichac97R3AttachInternal(PPDMDEVINS pDevIns, PAC97STATER3 pThisCC, uns
     int rc = PDMDevHlpDriverAttach(pDevIns, uLUN, &pThisCC->IBase, &pDrvBase, pDrv->szDesc);
     if (RT_SUCCESS(rc))
     {
-       pDrv->pConnector = PDMIBASE_QUERY_INTERFACE(pDrvBase, PDMIAUDIOCONNECTOR);
+        pDrv->pConnector = PDMIBASE_QUERY_INTERFACE(pDrvBase, PDMIAUDIOCONNECTOR);
         AssertPtr(pDrv->pConnector);
         if (RT_VALID_PTR(pDrv->pConnector))
         {
@@ -4271,7 +4271,7 @@ static int ichac97R3AttachInternal(PPDMDEVINS pDevIns, PAC97STATER3 pThisCC, uns
             LogFunc(("LUN#%u: returns VINF_SUCCESS (pCon=%p)\n", uLUN, pDrv->pConnector));
             return VINF_SUCCESS;
         }
-        RTMemFree(pDrv);
+
         rc = VERR_PDM_MISSING_INTERFACE_BELOW;
     }
     else if (rc == VERR_PDM_NO_ATTACHED_DRIVER)

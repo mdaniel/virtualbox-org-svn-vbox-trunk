422623d18ad2d2044841471c2b722c3c14e58111
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-04-23 11:48:48 +0000

    IPRT/ldrNative-posix.cpp: Comment about the dlerror problem from bugref:10892 / ticketref:22193.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@109061 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/posix/ldrNative-posix.cpp
+++ src/VBox/Runtime/r3/posix/ldrNative-posix.cpp
@@ -96,6 +96,8 @@ DECLHIDDEN(int) rtldrNativeLoad(const char *pszFilename, uintptr_t *phHandle, ui
         return VINF_SUCCESS;
     }
 
+    /* Note! In a hardened binary, dlerror may return NULL or obsolete information
+             because we intercept dlopen and dlmopen calls. See @bugref{109892}. */
     const char *pszDlError = dlerror();
     RTErrInfoSet(pErrInfo, VERR_FILE_NOT_FOUND, RT_VALID_PTR(pszDlError) ? pszDlError : "unknown dlopen error");
     LogRel(("rtldrNativeLoad: dlopen('%s', RTLD_NOW | RTLD_LOCAL) failed: %s\n", pszFilename, pszDlError));

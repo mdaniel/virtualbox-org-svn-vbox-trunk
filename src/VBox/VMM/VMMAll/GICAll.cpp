6181aa01797f5870fa82c975c5b14769dbe6a247
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-05-05 07:59:26 +0000

    VMM/GIC: bugref:10877 Fix buggy assertion checking MBIs and LPI support go hand-in-hand.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@109142 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/GICAll.cpp
+++ src/VBox/VMM/VMMAll/GICAll.cpp
@@ -2111,8 +2111,8 @@ DECLINLINE(VBOXSTRICTRC) gicDistReadRegister(PPDMDEVINS pDevIns, PVMCPUCC pVCpu,
                 *puValue |= GIC_DIST_REG_TYPER_LPIS
                          |  GIC_DIST_REG_TYPER_NUM_LPIS_SET(pGicDev->uMaxLpi);
             }
-            Assert((*puValue &  (GIC_DIST_REG_TYPER_MBIS | GIC_DIST_REG_TYPER_LPIS))
-                             == (GIC_DIST_REG_TYPER_MBIS | GIC_DIST_REG_TYPER_LPIS));
+            AssertMsg(   RT_BOOL(*puValue & GIC_DIST_REG_TYPER_MBIS)
+                      == RT_BOOL(*puValue & GIC_DIST_REG_TYPER_LPIS), ("%#RX32\n", *puValue));
             break;
         }
         case GIC_DIST_REG_PIDR2_OFF:

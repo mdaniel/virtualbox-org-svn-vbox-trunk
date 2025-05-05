9f9998ae65e837613f42a471dc0b7530f87aa9c9
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-05-05 14:22:41 +0000

    FE/Qt: bugref:10887 Do not recursively scan loaded host folder. That is not very clever.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@109156 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoContentBrowser.cpp
+++ src/VBox/Frontends/VirtualBox/src/medium/viso/UIVisoContentBrowser.cpp
@@ -975,7 +975,7 @@ void UIVisoContentBrowser::createLoadedFileEntries(const QMap<QString, QString> 
                     pItem->setData(strLocalPath, UIFileSystemModelData_LocalPath);
                 /* Pre-scan and populate the directory since we may need its content while processing removed items: */
                 if (enmObjectType == KFsObjType_Directory)
-                    scanHostDirectory(pItem, true  /* recursive */);
+                    scanHostDirectory(pItem, false  /* recursive */);
             }
             if (i == pathList.size() - 1)
                 createVisoEntry(pItem->path(), pItem->data(UIFileSystemModelData_LocalPath).toString(), false);
